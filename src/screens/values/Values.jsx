959645591854a4b0a44324173e9986db215591a1
Emmy britt <beritogwu@gmail.com>
2022-12-26 14:34:04 +0100

    updates:  Values is mobile responsive

--- src/screens/values/Values.jsx
+++ src/screens/values/Values.jsx
@@ -6,21 +6,21 @@ const Values = () => {
     <Container noGutter flex justify="center" className="py-20 bg-white">
       <Container
         as="div"
-        flex
+        flex="lg"
         noGutter
         justify="evenly"
-        className="bg-[#671E78] py-10 rounded-xl w-4/5 pl-20"
+        className="bg-[#671E78] py-10 lg:rounded-xl lg:w-4/5 lg:pl-20 px-5"
       >
-        <div className="border-r pr-8 border-dashed">
-          <div className="h-[80px] w-[80px] mb-6 bg-white/20 rounded-lg"></div>
+        <div className="lg:border-r pr-8 border-dashed mb-4 lg:mb-0">
+          <div className="h-[80px] w-[80px] mb-6 bg-white/20 relative before:content-[' '] before:w-full before:h-0 before:absolute before:left-0 before:bottom-0 before:border-t-[29px] before:border-r-[29px] before:border-white/20"></div>
           <Typography className="text-white mb-2">Quality Education</Typography>
-          <Typography className="text-[18px] leading-[30px] opacity-50 w-72 text-white">
+          <Typography className="text-[18px] leading-[30px] opacity-50 lg:w-72 text-white">
             End-to-end payments and financial management in a single solution.
             Meet the right platform.
           </Typography>
         </div>
 
-        <div className="pl-5 border-r pr-8 border-dashed">
+        <div className="lg:pl-5 lg:border-r pr-8 border-dashed mb-4 lg:mb-0">
           <div className="h-[80px] w-[80px] mb-6 bg-white/20"></div>
           <Typography className="text-white mb-2">Quality Education</Typography>
           <Typography className="text-[18px] leading-[30px] opacity-50 w-72 text-white">
@@ -29,7 +29,7 @@ const Values = () => {
           </Typography>
         </div>
 
-        <div className="pl-5 pr-8">
+        <div className="lg:pl-5 pr-8">
           <div className="h-[80px] w-[80px] mb-6 bg-white/20"></div>
           <Typography className="text-white mb-2">Quality Education</Typography>
           <Typography className="text-[18px] leading-[30px] opacity-50 w-72 text-white">
